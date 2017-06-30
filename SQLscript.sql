ad002c4ea8f90922a31f40e522c150bda98d4bb5
YuriyDzyadyk <yuraa_@ukr.net>
2017-06-30 19:17:38 +0300

    SQL script

---
+++ SQLscript.sql
@@ -0,0 +1,11 @@
+drop database if exists phpDB;
+create database phpDB;
+use phpDB;
+CREATE TABLE `users` (
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`userID`)
+);
+
